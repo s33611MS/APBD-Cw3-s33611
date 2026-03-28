9783ebb539722bea0d64d9536e39811da27bf7e1
s33611MS <s33611@pjwstk.edu.pl>
2026-03-28 21:38:02 +0100

    add zadanie02

--- Exercises/ZadaniaLinq.cs
+++ Exercises/ZadaniaLinq.cs
@@ -31,7 +31,7 @@ public sealed class ZadaniaLinq
     /// </summary>
     public IEnumerable<string> Zadanie02_AdresyEmailStudentow()
     {
-        throw Niezaimplementowano(nameof(Zadanie02_AdresyEmailStudentow));
+       return DaneUczelni.Studenci.Select(s => $"{s.Email}");
     }
 
     /// <summary>
