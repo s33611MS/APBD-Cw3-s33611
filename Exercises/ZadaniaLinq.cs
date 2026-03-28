24cdf85a290f7d07abc8300abe4c31671fa2a031
s33611MS <s33611@pjwstk.edu.pl>
2026-03-28 21:45:08 +0100

    add zadanie03

--- Exercises/ZadaniaLinq.cs
+++ Exercises/ZadaniaLinq.cs
@@ -46,7 +46,7 @@ public sealed class ZadaniaLinq
     /// </summary>
     public IEnumerable<string> Zadanie03_StudenciPosortowani()
     {
-        throw Niezaimplementowano(nameof(Zadanie03_StudenciPosortowani));
+        return DaneUczelni.Studenci.OrderBy(s => s.Nazwisko).ThenBy(s => s.Imie).Select(s => $"{s.NumerIndeksu} {s.Imie} {s.Nazwisko}");
     }
 
     /// <summary>
