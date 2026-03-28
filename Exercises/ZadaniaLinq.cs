8ac95c36aaaa9acb63c8d6b8af09e5692f574f0d
s33611MS <s33611@pjwstk.edu.pl>
2026-03-28 23:35:04 +0100

    add zadanie07

--- Exercises/ZadaniaLinq.cs
+++ Exercises/ZadaniaLinq.cs
@@ -118,7 +118,8 @@ public sealed class ZadaniaLinq
     /// </summary>
     public IEnumerable<string> Zadanie07_LiczbaAktywnychZapisow()
     {
-        throw Niezaimplementowano(nameof(Zadanie07_LiczbaAktywnychZapisow));
+        return [DaneUczelni.Zapisy
+            .Count(z => z.CzyAktywny).ToString()];
     }
 
     /// <summary>
